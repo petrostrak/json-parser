4f934ebcac18a2ed47a6292e75a6213a873c688a
petrostrak <pit.trak@gmail.com>
2023-09-30 15:17:04 +0300

    add read_char method

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -24,4 +24,14 @@ impl Lexer {
             ch: '0',
         }
     }
+
+    pub fn read_char(&mut self) {
+        if self.read_position >= self.input.len() {
+            self.ch = '0'
+        } else {
+            self.ch = self.input[self.read_position];
+        }
+        self.position = self.read_position;
+        self.read_position = self.read_position + 1;
+    }
 }
