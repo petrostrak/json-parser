42f0986d13c0e92ccacf52d5f6feefd9f32bfb08
petrostrak <pit.trak@gmail.com>
2023-10-01 17:19:58 +0300

    add more tokens and implement next_token method

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -41,4 +41,48 @@ impl Lexer {
             self.read_char();
         }
     }
+
+    pub fn next_token(&mut self) -> token::Token {
+        let read_identifier = |l: &mut Lexer| -> Vec<char> {
+            let position = l.position;
+            while l.position <= l.input.len() && is_letter(l.ch) {
+                l.read_char();
+            }
+            l.input[position..l.position].to_vec()
+        };
+
+        let read_number = |l: &mut Lexer| -> Vec<char> {
+            let position = l.position;
+            while l.position <= l.input.len() && is_digit(l.ch) {
+                l.read_char();
+            }
+            l.input[position..l.position].to_vec()
+        };
+
+        let tok: token::Token;
+        self.skip_whitespace();
+        match self.ch {
+            ':' => token::Token::COLON(self.ch),
+            ',' => token::Token::COMMA(self.ch),
+            '{' => token::Token::LBRACE(self.ch),
+            '}' => token::Token::RBRACE(self.ch),
+            '[' => token::Token::RBRACKET(self.ch),
+            ']' => token::Token::RBRACKET(self.ch),
+            '"' => token::Token::QUOTATION(self.ch),
+            '0' => token::Token::EOF,
+            _ => {
+                if is_letter(self.ch) {
+                    let ident = read_identifier(self);
+                    return token::get_keyword_token(&ident);
+                } else if is_digit(self.ch) {
+                    let ident = read_number(self);
+                    return token::Token::INT(ident);
+                } else {
+                    return token::Token::ILLEGAL;
+                }
+            }
+        }
+        self.read_char();
+        tok
+    }
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -2,20 +2,24 @@
 pub enum Token {
     ILLEGAL,
     EOF,
+    INT(Vec<char>),
     IDENT(Vec<char>),
     COLON(char),
     COMMA(char),
     LBRACE(char),
     RBRACE(char),
+    LBRACKET(char),
+    RBRACKET(char),
+    QUOTATION(char),
     TRUE,
     FALSE,
 }
 
-pub fn get_keyword_token(ident: &Vec<char>) -> Result<Token, String> {
+pub fn get_keyword_token(ident: &Vec<char>) -> Token {
     let identifier = ident.into_iter().collect::<String>();
     match &identifier[..] {
-        "true" => Ok(Token::TRUE),
-        "false" => Ok(Token::FALSE),
-        _ => Err(String::from("Not a keyword")),
+        "true" => Token::TRUE,
+        "false" => Token::FALSE,
+        _ => Token::ILLEGAL,
     }
 }
