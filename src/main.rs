6cd1469e3d97fd0c13fb612dc95870572fbfa16c
petrostrak <pit.trak@gmail.com>
2023-10-01 17:53:38 +0300

    implement json lexer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 mod lexer;
 
 fn main() {
-    println!("Hello, world!");
+    let json = String::from("{\"name\": \"Petros\", \"age\": 37}");
+
+    let mut l = lexer::Lexer::new(json.chars().collect());
+    l.read_char();
+    loop {
+        let token = l.next_token();
+        if token == lexer::token::Token::EOF {
+            break;
+        } else {
+            println!("{token:?}");
+        }
+    }
+    println!("{} {} {}", char::from(l.ch), l.position, l.read_position);
 }
